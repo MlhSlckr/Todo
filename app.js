ff4fe4473f6d80c2f9f85f27b1b21965ce9ae0a7
MlhSlckr <selcukermelih@gmail.com>
2021-09-25 14:04:17 +0300

    edit update

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const input = document.querySelector(".input");
 const add = document.querySelector(".add");
-const edit = document.querySelector(".edit");
 const texts = document.querySelector(".texts");
-let deleteBtns;
 
+let removeBtns;
 let inputValue;
 let isCompleted;
+
+add.addEventListener("click", () => {
+  if (input.value === "") {
+    input.style.border = "1px solid red";
+    setTimeout(() => {
+      input.style.border = "none";
+    }, 1500);
+  } else {
+    createDiv();
+    input.value = "";
+  }
+});
 
 function createDiv() {
   const todoDiv = document.createElement("div");
@@ -16,7 +27,6 @@ function createDiv() {
 
   const todoCb = document.createElement("input");
   todoCb.type = "checkbox";
-  todoCb.checked = isCompleted;
   todoCb.classList.add("todo_cb");
 
   const todoText = document.createElement("span");
@@ -33,11 +43,13 @@ function createDiv() {
   remove.classList.add("far");
   remove.classList.add("fa-trash");
   remove.classList.add("todo_delete");
+  remove.addEventListener("click", deleteTodo);
 
   const edit = document.createElement("i");
   edit.classList.add("far");
   edit.classList.add("fa-edit");
   edit.classList.add("todo_edit");
+  edit.addEventListener("click", editTodo);
 
   todoRight.appendChild(remove);
   todoRight.appendChild(edit);
@@ -48,22 +60,63 @@ function createDiv() {
   texts.appendChild(todoDiv);
 }
 
-add.addEventListener("click", () => {
-  if (input.value === "") {
-    input.style.border = "1px solid red";
-    setTimeout(() => {
-      input.style.border = "none";
-    }, 1500);
-  } else {
-    createDiv();
-    deleteBtns = document.querySelectorAll(".todo_delete");
-  }
-});
-
-deleteBtns.forEach((btn) => btn.addEventListener("click", deleteTodo()));
-
 const deleteTodo = (e) => {
   const todo = e.target.parentElement.parentElement;
   todo.remove();
-  console.log("q");
+};
+
+const editTodo = (e) => {
+  const todo = e.target.parentElement.parentElement;
+  const text = todo.firstChild.children[1].textContent;
+  const editInput = document.createElement("input");
+  editInput.type = "text";
+  editInput.value = text;
+  editInput.classList.add("edit_input");
+  document.querySelector(".todo_left").appendChild(editInput);
+  document.querySelector(".todo_text").remove();
+  document.querySelector(".todo_cb").remove();
+  document.querySelector(".todo_delete").remove();
+  document.querySelector(".todo_edit").remove();
+  const saveBtn = document.createElement("i");
+  saveBtn.classList.add("far");
+  saveBtn.classList.add("fa-save");
+  saveBtn.classList.add("save");
+  document.querySelector(".todo_right").appendChild(saveBtn);
+  saveBtn.addEventListener("click", save);
+};
+
+const save = (e) => {
+  const newText = document.querySelector(".edit_input").value;
+  if (document.querySelector(".edit_input").value === "") {
+    document.querySelector(".edit_input").style.border = "1px solid red";
+  } else {
+    const todoCb = document.createElement("input");
+    todoCb.type = "checkbox";
+    todoCb.classList.add("todo_cb");
+
+    const todoText = document.createElement("span");
+    todoText.classList.add("todo_text");
+
+    document.querySelector(".todo_left").appendChild(todoCb);
+    document.querySelector(".todo_left").appendChild(todoText);
+
+    const remove = document.createElement("i");
+    remove.classList.add("far");
+    remove.classList.add("fa-trash");
+    remove.classList.add("todo_delete");
+    remove.addEventListener("click", deleteTodo);
+
+    const edit = document.createElement("i");
+    edit.classList.add("far");
+    edit.classList.add("fa-edit");
+    edit.classList.add("todo_edit");
+    edit.addEventListener("click", editTodo);
+
+    document.querySelector(".todo_right").appendChild(remove);
+    document.querySelector(".todo_right").appendChild(edit);
+
+    document.querySelector(".save").remove();
+    document.querySelector(".edit_input").remove();
+    todoText.textContent = newText;
+  }
 };
