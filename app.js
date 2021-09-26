e7c53f6d5301cd41762c5ab7e3532a9b66ed7f4b
MlhSlckr <selcukermelih@gmail.com>
2021-09-26 21:54:40 +0300

    q

--- app.js
+++ app.js
@@ -18,81 +18,79 @@ add.addEventListener("click", () => {
   }
 });
 
-function createDiv() {
+const createDiv = () => {
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("note");
 
   const todoLeft = document.createElement("div");
   todoLeft.classList.add("todo_left");
 
-  const todoCb = document.createElement("input");
-  todoCb.type = "checkbox";
-  todoCb.classList.add("todo_cb");
+  const todoRight = document.createElement("div");
+  todoRight.classList.add("todo_right");
 
   const todoText = document.createElement("span");
   todoText.classList.add("todo_text");
   todoText.textContent = input.value;
 
+  const todoCb = document.createElement("input");
+  todoCb.classList.add("todo_cb");
+  todoCb.type = "checkbox";
+  todoCb.addEventListener("click", completed);
+
   const editInput = document.createElement("input");
   editInput.classList.add("todo_editInput");
-  editInput.defaultValue = todoText.textContent;
+  editInput.defaultValue = input.value;
 
-  todoLeft.appendChild(todoCb);
-  todoLeft.appendChild(todoText);
-  todoLeft.appendChild(editInput);
-
-  const todoRight = document.createElement("div");
-  todoRight.classList.add("todo_right");
-
-  const remove = document.createElement("i");
-  remove.classList.add("far");
-  remove.classList.add("fa-trash");
-  remove.classList.add("todo_delete");
-  remove.addEventListener("click", deleteTodo);
-
-  const edit = document.createElement("i");
-  edit.classList.add("far");
-  edit.classList.add("fa-edit");
-  edit.classList.add("todo_edit");
-  edit.addEventListener("click", editTodo);
+  const deleteBtn = document.createElement("i");
+  deleteBtn.classList.add("far");
+  deleteBtn.classList.add("fa-trash");
+  deleteBtn.classList.add("todo_delete");
+  deleteBtn.addEventListener("click", deleteTodo);
 
   const saveBtn = document.createElement("i");
   saveBtn.classList.add("far");
   saveBtn.classList.add("fa-save");
   saveBtn.classList.add("todo_save");
-  saveBtn.addEventListener("click", saveTodo);
+  saveBtn.addEventListener("click", save);
 
-  todoRight.appendChild(remove);
-  todoRight.appendChild(edit);
+  const editBtn = document.createElement("i");
+  editBtn.classList.add("far");
+  editBtn.classList.add("fa-edit");
+  editBtn.classList.add("todo_edit");
+  editBtn.addEventListener("click", edit);
+
+  todoLeft.appendChild(todoCb);
+  todoLeft.appendChild(todoText);
+  todoLeft.appendChild(editInput);
+
+  todoRight.appendChild(deleteBtn);
+  todoRight.appendChild(editBtn);
   todoRight.appendChild(saveBtn);
 
   todoDiv.appendChild(todoLeft);
   todoDiv.appendChild(todoRight);
 
   texts.appendChild(todoDiv);
-}
+};
 
 const deleteTodo = (e) => {
   const todo = e.target.parentElement.parentElement;
   todo.remove();
 };
 
-const editTodo = (e) => {
+const edit = (e) => {
   const todo = e.target.parentElement.parentElement;
   todo.classList.add("edited");
 };
 
-const saveTodo = (e, editInput) => {
+const save = (e) => {
   const todo = e.target.parentElement.parentElement;
   const newText = todo.firstChild.children[2].value;
-  if (todo.firstChild.children[2].value === "") {
-    editInput.style.border = "1px solid red";
-    console.log("q");
-    setTimeout(() => {
-      editInput.style.border = "";
-    }, 1500);
-  } else {
-    todo.firstChild.children[1].textContent = newText;
-    todo.classList.remove("edited");
-  }
+  todo.firstChild.children[1].textContent = newText;
+  todo.classList.remove("edited");
+};
+
+const completed = (e) => {
+  const todo = e.target.parentElement.parentElement;
+  todo.classList.toggle("completed");
 };
